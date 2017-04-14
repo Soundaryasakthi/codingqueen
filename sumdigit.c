429188cbe0abd80fc4b0c858f2cee2377ddf8b25
Soundaryasakthi <soundaryasakthi14@gmail.com>
2017-04-14 13:15:19 +0530

    Create sumdigit.c

---
+++ sumdigit.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int a,sum=0,remainder;
+printf("Enter the number\n");
+scanf("%d",&a);
+while(n!=0)
+{
+remainder=a%10;
+sum=sum+remainder;
+a=a/10;
+}
+printf("sum of digits");
+return 0;
+}
